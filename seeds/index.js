6e7b1578b94b2bbad4adadcf4d77a1b0848a14a7
Andres <andresdomlez@gmail.com>
2021-08-20 20:28:39 +0100

    Authorization

--- seeds/index.js
+++ seeds/index.js
@@ -23,7 +23,7 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
-      author: '611d74cca417bf268b64e48f',
+      author: '611fe2e6fa2a7608f06155d5',
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       image: 'https://source.unsplash.com/collection/483251',
